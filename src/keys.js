36013391c013d944c28f6649f126cb99316a6b48
Shloka Jain <shlokajain@Shlokas-MacBook-Air.local>
2020-12-01 14:50:50 +0530

    Initial Commit

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCtKJD2-kC5ety57G1RndELOX_dE6dwjVQ";
+
+export default API_KEY;
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import API_KEY from './keys';
+
+const CONTEXT_KEY = "1b32ab47d8e4f36f3";
+
+const useGoogleSearch = (term) => {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const fetchData = async() => {
+            fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`)
+            .then(response => response.json())
+            .then(result => {
+                setData(result)
+            })
+        }
+        fetchData();
+    }, [term])
+    return { data }
+}
+
+export default useGoogleSearch;
